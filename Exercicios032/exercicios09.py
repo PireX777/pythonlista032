cb38cf9f633fbaa5e24a56b6974a27bcd5c8c594
Raphael Martins <raphael.m.nascimento6@aluno.senai.br>
2023-08-14 10:01:47 -0300

    atividades 032

--- Exercicios032/exercicios09.py
+++ Exercicios032/exercicios09.py
@@ -1,12 +1,9 @@
 '''9) Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas
 em dias. Obs: Considere os anos com 365 dias e os meses com 30 dias'''
+idade = int(input("Informe sua idade em anos: "))
+idade2 = int(input("Informe sua idade em meses: "))
+idade3 = int(input("Informe sua idade em dias: "))
 
-id = int(input("Informe sua idade em anos: "))
-id2 = int(input("Informe sua idade em meses: "))
-id3 = int(input("Informe sua idade em dias: "))
+total_dias = (idade * 365) + (idade2 * 30) + idade3
 
-id4 = 365 * id
-ida2 = 30 * id2
-idt = id4 + ida2 + id3
-
-print(f"O valor de sua idade em dias: {idt}")
+print(f"O valor de sua idade em dias: {total_dias}")
